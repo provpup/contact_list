c9487344eced230301c242f35736bbe05e2e77b5
Kenny Teng <provpup@users.noreply.github.com>
2015-03-11 21:31:57 +0000

    Use FactoryGirl in routes rspec test

--- spec/02_contact_http_routes.rb
+++ spec/02_contact_http_routes.rb
@@ -4,16 +4,11 @@ RSpec.describe 'Contact List API routes' do
   include RackTestMixin
 
   before :all do
-    @sam = Contact.create(firstname: 'Sam', lastname: 'Hudson', email: 'sam.hudson@abcdef.com')
-    Contact.create(firstname: 'Sonia', lastname: 'Harris', email: 'sonia.harris@abcdef.com')
-    Contact.create(firstname: 'Emma', lastname: 'Paterson', email: 'emma.paterson@abcdef.com')
-    Contact.create(firstname: 'Sue', lastname: 'Jackson', email: 'sue.jackson@abcdef.com')
-    Contact.create(firstname: 'Connor', lastname: 'Hughes', email: 'connor.hughes@abcdef.com')
-    Contact.create(firstname: 'Theresa', lastname: 'North', email: 'theresa.north@abcdef.com')
-    Contact.create(firstname: 'Stephen', lastname: 'Gibson', email: 'stephen.gibson@abcdef.com')
-    Contact.create(firstname: 'Zoe', lastname: 'Harris', email: 'zoe.harris@abcdef.com')
-    Contact.create(firstname: 'Steven', lastname: 'Short', email: 'steven.short@abcdef.com')
-    @dorothy = Contact.create(firstname: 'Dorothy', lastname: 'Walsh', email: 'dorothy.walsh@abcdef.com')
+    10.times do
+      FactoryGirl.create :contact
+    end
+    @sam = Contact.first
+    @dorothy = Contact.last
   end
   
   it 'should allow accessing the home page' do
