0800fcc60ba6e7c26d963a3b9f868ee721a1f627
Kenny Teng <provpup@users.noreply.github.com>
2015-03-11 22:20:20 +0000

    Update table in index page via JQuery getJSON call to /contacts

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var updateTable = function(jsonArray) {
+    var contactNode = $('#contacts');
+    contactNode.empty();
+    for (var i = 0; i < jsonArray.length; i++) {
+      var rowNode = $('<tr>');
+      rowNode.append($('<td>').text(jsonArray[i]['firstname']));
+      rowNode.append($('<td>').text(jsonArray[i]['lastname']));
+      rowNode.append($('<td>').text(jsonArray[i]['email']));
+      rowNode.append($('<td>').text(jsonArray[i]['phonenumber']));
+      contactNode.append(rowNode);
+    }
+  };
+
+  $.getJSON('/contacts').done(updateTable);
 });
